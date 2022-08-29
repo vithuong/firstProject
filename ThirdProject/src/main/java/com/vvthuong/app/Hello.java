67a53669572ea3978f1cbfb6c143078121853d31
Vi Văn Thường <65748806+vithuong@users.noreply.github.com>
2022-08-30 04:52:51 +0700

    %s %d Hello

---
+++ ThirdProject/src/main/java/com/vvthuong/app/Hello.java
@@ -0,0 +1,12 @@
+package com.vvthuong.app;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Vi");
+		System.out.println("Thuong");
+		
+		String s = "Vi Thuong";
+		int d = 100;
+		System.out.printf("%s can nang la %d kg", s, d);
+	}
+}
