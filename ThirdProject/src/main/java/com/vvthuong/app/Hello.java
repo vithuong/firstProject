733c092cd6405cb8f7ef58ed328f2cea3b6eaf62
Vi Văn Thường <65748806+vithuong@users.noreply.github.com>
2022-08-30 05:32:21 +0700

    Jcore: Scanner, println, printf, nextLine, nextDouble.

--- ThirdProject/src/main/java/com/vvthuong/app/Hello.java
+++ ThirdProject/src/main/java/com/vvthuong/app/Hello.java
@@ -1,16 +1,33 @@
 package com.vvthuong.app;
 
+import java.util.Scanner;
+
 public class Hello {
 	public static void main(String[] args) {
-		hello();//fix error because Static method only work with static method.
+		//hello();//fix error because Static method only work with static method.
 		//vua roi la to chuc theo huong thu tuc, tu ham nay ta goi ham khac.
+		input();
+	}
+	private static void input() {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print(">>Name: ");
+		String fullname = scanner.nextLine();
+		
+		System.out.print(">>Salary: ");
+		double salary = scanner.nextDouble();
+		
+		System.out.print(">>Age: ");
+		int age = scanner.nextInt();
+		
+		System.out.printf("Input done: %s, %f, %d", fullname, salary, age);
 	}
 	@SuppressWarnings("unused")
 	private static void hello() {
 		System.out.println("Vi");
-		System.out.println("Thuong");
+		System.out.println("Thường");
 		
-		String s = "Vi Thuong";
+		String s = "Vi Thường";
 		int d = 100;
 		System.out.printf("%s can nang la %d kg", s, d);
 
