d233804a98f249b9570258d56d6eb4d6a8d379a1
Vi Văn Thường <65748806+vithuong@users.noreply.github.com>
2022-08-30 04:59:18 +0700

    Error: Static method only work with static method

--- ThirdProject/src/main/java/com/vvthuong/app/Hello.java
+++ ThirdProject/src/main/java/com/vvthuong/app/Hello.java
@@ -2,11 +2,15 @@ package com.vvthuong.app;
 
 public class Hello {
 	public static void main(String[] args) {
+		hello();//error because Static method only work with static method.
+	}
+	private void hello() {
 		System.out.println("Vi");
 		System.out.println("Thuong");
 		
 		String s = "Vi Thuong";
 		int d = 100;
 		System.out.printf("%s can nang la %d kg", s, d);
+
 	}
 }
