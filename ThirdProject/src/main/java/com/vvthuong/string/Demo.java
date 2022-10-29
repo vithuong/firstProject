8962d1732ffb826bb31fae8d5467898e20b81e39
Vi Văn Thường <65748806+vithuong@users.noreply.github.com>
2022-10-29 13:44:08 +0700

    jCore: demo4 Kiem tra email & bien so xe may Sai Gon.

--- ThirdProject/src/main/java/com/vvthuong/string/Demo.java
+++ ThirdProject/src/main/java/com/vvthuong/string/Demo.java
@@ -5,24 +5,36 @@ import java.util.Scanner;
 public class Demo {
 	public static void main(String[] args) {
 		// demo1();
-		//demo2();
-		demo3();
+		// demo2();
+		// demo3();
+		demo4();
 	}
 
-	
+	private static void demo4() {
+		// kiem tra email
+		// String s = "abc.def.ijk.lmn.opw.twx-yz.com";
+		String s = "abc.def.ijk.lmn.opw.twx@yz.com";
+		String reEmail = "\\w+(\\.\\w+)+@\\w+(\\.\\w+){1,2}";
+		System.out.println(s.matches(reEmail));
+		// kiem tra bien so xe sai gon
+		String s1 = "52A3-123.45";
+		String reEmail1 = "5\\d[A-Z]\\d-((\\d{4})|(\\d{3}\\.\\d{2}))";
+		System.out.println(s1.matches(reEmail1));
+
+	}
 
 	private static void demo3() {
 		String[] names = new String[5];
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("vui long nhap ho va ten 5 nguoi: ");
 		for (int i = 0; i < names.length; i++) {
 			names[i] = scanner.nextLine();
-			
+
 		}
 		// xuat hovaten cua nhung nguoi co ho la nguyen va ten la tuan
 		for (String name : names) {
-			if (name.startsWith("Nguyen")||name.endsWith("Tuan")) {
+			if (name.startsWith("Nguyen") || name.endsWith("Tuan")) {
 				System.out.println(name.toUpperCase());
 			}
 		}
@@ -32,26 +44,23 @@ public class Demo {
 				String nameOnly = name.substring(name.lastIndexOf(" ") + 1);
 			}
 		}
-		
+
 	}
-
-
 
 	private static void demo2() {
 		String s = "1,2'3' 5,6,7";
 		String[] ss = s.split("[,' ]+");
 		for (String a : ss) {
 			int b = Integer.parseInt(a);
-			//System.out.println(b);
-			// tach chuoi s cho vao arrray, sau do duyet array, chuyen string sang int va lay so le
+			// System.out.println(b);
+			// tach chuoi s cho vao arrray, sau do duyet array, chuyen string sang int va
+			// lay so le
 			if (b % 2 == 1) {
 				System.out.println(b);
 			}
-			
+
 		}
 	}
-
-
 
 	private static void demo1() {
 		String s = "Xuat thong tin hinh chu nhat ra man hinh    ";
