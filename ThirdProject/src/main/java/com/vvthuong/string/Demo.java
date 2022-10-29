1c7390a6c0504f4f281ae76b3ecbf9bcbc3d15ca
Vi Văn Thường <65748806+vithuong@users.noreply.github.com>
2022-10-29 11:48:35 +0700

    jCore: demo2 tach chuoi s cho vao arrray va lay so le ra

--- ThirdProject/src/main/java/com/vvthuong/string/Demo.java
+++ ThirdProject/src/main/java/com/vvthuong/string/Demo.java
@@ -2,12 +2,31 @@ package com.vvthuong.string;
 
 public class Demo {
 	public static void main(String[] args) {
-		demo1();
+		// demo1();
+		demo2();
 	}
+
+	
+
+	private static void demo2() {
+		String s = "1,2'3' 5,6,7";
+		String[] ss = s.split("[,' ]+");
+		for (String a : ss) {
+			int b = Integer.parseInt(a);
+			//System.out.println(b);
+			// tach chuoi s cho vao arrray, sau do duyet array, chuyen string sang int va lay so le
+			if (b % 2 == 1) {
+				System.out.println(b);
+			}
+			
+		}
+	}
+
+
 
 	private static void demo1() {
 		String s = "Xuat thong tin hinh chu nhat ra man hinh    ";
-		
+
 		System.out.println(">> Chieu Dai Leng(): " + s.length());
 		System.out.println(">> Hoa Thuong toUpperCase: " + s.toUpperCase() + " >> " + s.toLowerCase());
 		System.out.println(">> cat bo khoang trang Trim(): " + s.trim());
@@ -15,9 +34,7 @@ public class Demo {
 		System.out.println(">> co chua contains : " + s.contains("hinh"));
 		System.out.println(">> indexOf : " + s.indexOf("hinh"));
 		System.out.println(">> Last indexOf : " + s.lastIndexOf("hinh"));
-		System.out.println(">> Start With: " + s.startsWith("Xuat"));
-		
-		
-		
+		System.out.println(">> Start With: " + s.startsWith("thong"));
+
 	}
 }
